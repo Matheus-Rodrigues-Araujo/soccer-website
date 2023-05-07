6ef69eb7790f0b67f4ec3d04915e8abc71fd451f
Matheus Rodrigues Araujo <matheus.rodgs.araujo13@gmail.com>
2023-05-07 13:43:47 -0300

    Create connection with axios for the routes and finish the design for some elements in the screen for the user experience

---
+++ src/routes/apiRoutes.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+// import  Axios  from "axios"
+
+// LEAGUES
+export const getLeagues = async () =>{
+
+    const options = {
+        method: 'GET',
+        url: 'https://api-football-v1.p.rapidapi.com/v3/leagues',
+        headers: {
+          'X-RapidAPI-Key': 'a44ca3f124mshf5256df877ee8a2p16ed0djsn312ab59cbb57',
+          'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+        }
+      };
+
+      try {
+        const response = await axios.get(options)
+        console.log(response.data)
+      } catch (error) {
+        console.log(error)
+      }
+}
+
+
+
+
+// TEAMS
+// PLAYERS 
+// MATCHES
+ 
